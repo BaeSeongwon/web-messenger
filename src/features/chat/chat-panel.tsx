b0ab910e87f9fac02d8a172299fe0a2ad4f68e54
BaeSeongwon <seongwon9179@gmail.com>
2026-07-15 18:44:17 +0900

    feat: 패널 와이어프레임 세분화 및 UI 컴포넌트 기반 구성

--- src/features/chat/chat-panel.tsx
+++ src/features/chat/chat-panel.tsx
@@ -1,5 +1,18 @@
 function ChatPanel() {
-  return <div className="flex-1 bg-green-500">ChatPanel</div>
+  return (
+    <div className="flex flex-col flex-1 bg-white border-r border-layout">
+      {/* Header */}
+      <div className="flex w-full h-16 bg-white border-b border-layout p-4">
+        <div className="flex w-full h-full bg-red-100" role="chat-title" />
+        <div className="flex w-full h-full bg-blue-100" role="chat-context-menu" />
+      </div>
+      {/* Body */}
+      <div className="flex flex-col w-full flex-1 p-4" role="chat-body">
+        <div className="flex w-full h-full bg-green-100" role="chat-messages" />
+        <div className="flex w-full h-20 bg-yellow-100" role="chat-input-bar" />
+      </div>
+    </div>
+  )
 }
 
 export default ChatPanel
--- src/features/room/room-details-panel.tsx
+++ src/features/room/room-details-panel.tsx
@@ -1,5 +1,19 @@
 function RoomDetailsPanel() {
-  return <div className="min-w-side-panel bg-yellow-500">RoomDetailsPanel</div>
+  return (
+    <div className="min-w-side-panel bg-white flex flex-col">
+      {/* Header */}
+      <div className="flex w-full h-16 bg-white border-b border-layout p-4">
+      <div className="flex w-full h-full bg-red-100" role="room-title" />
+      <div className="flex w-full h-full bg-blue-100" role="room-context-menu" />
+      </div>
+      {/* Body */}
+      <div className="flex flex-col w-full flex-1 p-4" role="room-details-body">
+        <div className="flex w-full h-60 bg-green-100" role="room-details-info" />
+        <div className="flex w-full h-60 bg-blue-100" role="room-details-context-menu" />
+        <div className="flex w-full flex-1 bg-purple-100" role="room-details-members" />
+      </div>
+    </div>
+  )
 }
 
 export default RoomDetailsPanel
